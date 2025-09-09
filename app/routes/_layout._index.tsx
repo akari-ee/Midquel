00f282632ee4468f4bbeaf68b124fc094fca7dc7
coggiee <zentechie7@gmail.com>
2025-09-09 15:20:39 +0900

    필름 및 아카이브 섹션 컴포넌트를 레이아웃에 통합

--- app/routes/_layout._index.tsx
+++ app/routes/_layout._index.tsx
@@ -1,8 +1,8 @@
 import { appConfig } from "~/config/app-config";
 import BlurScaleOnScroll from "~/components/blur-scale-on-scroll";
 import type { Route } from "./+types/_layout._index";
-import { Link } from "react-router";
-import { routeConfig } from "~/config/route-config";
+import FilmSection from "~/components/film/film-section";
+import ArchivesSection from "~/components/archives/archives-section";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -21,6 +21,7 @@ export default function Home() {
           alt="banner"
           className="w-full h-full object-cover brightness-75"
         />
+
         {/* Divider Decoration */}
         <div className="absolute top-0 left-0 w-full h-full">
           <div className="absolute top-5 left-0 w-full h-[0.5px] bg-[#7c7c7c]/70" />
@@ -32,7 +33,7 @@ export default function Home() {
         {/* Title + Description */}
         <BlurScaleOnScroll
           className="absolute bottom-0 left-0 w-full h-full pb-20 pl-10 lg:pl-20 flex flex-col gap-5 justify-end"
-          blurSpeed={0.9}
+          blurSpeed={0.1}
         >
           <h1 className="text-6xl font-medium lg:text-9xl lg:font-normal tracking-tighter">
             {appConfig.serviceName}
@@ -44,33 +45,10 @@ export default function Home() {
       {/* Archives + Film 컨테이너 - 앞 레이어, Hero를 덮으며 올라옴 */}
       <div className="relative z-10 flex flex-col w-full items-center bg-background mt-[100dvh] px-5 pt-10 pb-20">
         {/* Archives Section */}
-        <section id="archives-section" className="w-full h-fit flex">
-          <div className="flex justify-between items-end uppercase w-full py-1">
-            <h2 className="text-3xl lg:text-[40px] font-medium tracking-tighter">
-              Selected Archives
-            </h2>
-            <Link
-              to={routeConfig.ARCHIVES.href}
-              className="underline hover:text-muted-foreground transition-colors duration-300 underline-offset-[6px] text-sm"
-            >
-              See All Archives
-            </Link>
-          </div>
-        </section>
+        <ArchivesSection />
+
         {/* Film Section */}
-        <section className="w-full h-fit flex">
-          <div className="flex justify-between items-end uppercase w-full py-1">
-            <h2 className="text-3xl lg:text-[40px] font-medium tracking-tighter">
-              Film
-            </h2>
-            <Link
-              to={routeConfig.FILM.href}
-              className="underline hover:text-muted-foreground transition-colors duration-300 underline-offset-[6px] text-sm"
-            >
-              See All films
-            </Link>
-          </div>
-        </section>
+        <FilmSection />
       </div>
     </main>
   );

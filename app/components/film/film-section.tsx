6df2475d1a81a875ddf5b42b24879c449a6be138
coggiee <zentechie7@gmail.com>
2025-09-09 15:20:10 +0900

    필름 카드 및 필름 섹션 컴포넌트 추가

---
+++ app/components/film/film-section.tsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router";
+import { mockConfig } from "~/config/mock-config";
+import { routeConfig } from "~/config/route-config";
+import FilmCard from "./film-card";
+
+export default function FilmSection() {
+  return (
+    <section className="w-full h-fit flex flex-col gap-5 pt-10">
+      <div className="flex justify-between items-end uppercase w-full py-1">
+        <h2 className="text-3xl lg:text-[40px] font-medium tracking-tighter">
+          Film
+        </h2>
+        <Link
+          to={routeConfig.FILM.href}
+          className="hidden md:block underline hover:text-muted-foreground transition-colors duration-300 underline-offset-[6px] text-sm"
+        >
+          See all films
+        </Link>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-x-3 gap-y-3">
+        {mockConfig.filmData.map((item) => (
+          <FilmCard item={item} />
+        ))}
+      </div>
+      <Link
+        to={routeConfig.FILM.href}
+        className="uppercase md:hidden underline hover:text-muted-foreground transition-colors duration-300 underline-offset-[6px] text-sm"
+      >
+        See all films
+      </Link>
+    </section>
+  );
+}

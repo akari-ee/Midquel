866030fb7c23f6120434f252555343feb6cc7933
coggiee <zentechie7@gmail.com>
2025-09-09 16:57:41 +0900

    모바일 메뉴 기능 추가

--- app/components/shared/navbar.tsx
+++ app/components/shared/navbar.tsx
@@ -3,27 +3,34 @@ import { Link } from "react-router";
 import { appConfig } from "~/config/app-config";
 import { format } from "date-fns";
 import { useNavbarBgOnSection } from "~/hooks/useNavbarBgOnSection";
-import { routeConfig } from "~/config/route-config";
 import ServiceLogo from "./service-logo";
+import { motion } from "motion/react";
+import { cn } from "~/lib/utils";
 
 export default function Navbar() {
   const hasBg = useNavbarBgOnSection({
     sectionId: "archives-section",
     offset: -200,
   });
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header
-      className={`h-fit px-6 py-5 fixed top-0 z-50 w-full transition-colors duration-500 ${
-        hasBg ? "bg-background/90 backdrop-blur" : "bg-transparent"
-      }`}
+    <motion.header
+      id="site-header"
+      className={cn(
+        `overflow-hidden px-6 py-5 fixed top-0 z-50 w-full duration-500 `,
+        menuOpen ? "bg-background" : hasBg ? "bg-background" : "bg-transparent"
+      )}
+      initial={{ height: 96 }}
+      animate={{ height: menuOpen ? "100vh" : 96 }}
+      transition={{ type: "spring", stiffness: 100, damping: 20 }}
     >
       <nav className="flex justify-between items-center text-[15px]">
         {/* 좌측 로고 */}
         <ServiceLogo />
 
         {/* 중앙 네비게이션 */}
-        <ul className="flex justify-center items-center gap-6 p-5 uppercase flex-1">
+        <ul className="justify-center items-center gap-6 p-5 uppercase flex-1 md:flex hidden">
           {appConfig.navItems.map(({ label, href }) => (
             <li key={href} className="hover-text-muted-foreground">
               <Link to={href} prefetch="viewport">
@@ -34,12 +41,56 @@ export default function Navbar() {
         </ul>
 
         {/* 우측 시간 */}
-        <div className="p-5 uppercase flex items-center justify-end flex-1 gap-1">
+        <div className="p-5 uppercase items-center justify-end flex-1 gap-1 hidden md:flex">
           <p>{appConfig.location.city}</p>
           <Clock />
         </div>
+
+        {/* 모바일 메뉴 */}
+        <motion.span
+          onClick={() => setMenuOpen((prev) => !prev)}
+          className="inline-block h-fit overflow-hidden cursor-pointer select-none md:hidden"
+          aria-controls="site-header"
+          initial={false}
+        >
+          {/* MENU */}
+          <motion.span
+            className="block"
+            animate={{ y: menuOpen ? "-100%" : "0%" }}
+            transition={{ type: "spring", stiffness: 100, damping: 20 }}
+          >
+            MENU
+          </motion.span>
+
+          {/* CLOSE */}
+          <motion.span
+            className="block -mt-[1.5em]"
+            initial={{ y: "100%" }}
+            animate={{ y: menuOpen ? "0%" : "100%" }}
+            transition={{ type: "spring", stiffness: 100, damping: 20 }}
+          >
+            CLOSE
+          </motion.span>
+        </motion.span>
       </nav>
-    </header>
+
+      {/* 모바일 메뉴바 컨텐츠 */}
+      <aside>
+        <ul className="flex flex-col gap-2 p-5 uppercase">
+          {appConfig.navItems.map(({ label, href }) => (
+            <li key={href} className="text-5xl font-medium">
+              <Link to={href} prefetch="viewport">
+                {label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="p-5 uppercase flex items-center justify-start gap-1">
+          <p>{appConfig.location.city}</p>
+          <Clock />
+        </div>
+      </aside>
+    </motion.header>
   );
 }
 

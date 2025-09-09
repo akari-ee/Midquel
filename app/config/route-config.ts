8c4dda25c01e69e51c70ee8368629bce983441a8
coggiee <zentechie7@gmail.com>
2025-09-09 15:19:30 +0900

    모의 데이터 구성 추가 및 라우트 설정 수정

--- app/config/route-config.ts
+++ app/config/route-config.ts
@@ -1,6 +1,14 @@
 export const routeConfig = {
   HOME: { href: "/", label: "Home" },
-  FILM: { href: "/film", label: "Film" },
-  ARCHIVES: { href: "/archives", label: "Archives" },
+  FILM: {
+    href: "/film",
+    label: "Film",
+    detail: (slug: string) => `/film/${slug}`,
+  },
+  ARCHIVES: {
+    href: "/archives",
+    label: "Archives",
+    detail: (slug: string) => `/archives/${slug}`,
+  },
   PROFILE: { href: "/profile", label: "Profile" },
 } as const;

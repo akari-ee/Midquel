2dd0556a1fc9106fa18819ada2f69d9431a9f331
coggiee <zentechie7@gmail.com>
2025-09-11 16:23:36 +0900

    FilmeRoute 수정

--- app/routes/_layout.film.tsx
+++ app/routes/_layout.film.tsx
@@ -1,10 +1,5 @@
 import { Outlet } from "react-router";
 
-export const handle = {
-  navTargetSectionId: "film-section",
-  navOffset: 0,
-};
-
 export default function FilmLayoutRoute() {
   return (
     <main className="relative w-full h-full">

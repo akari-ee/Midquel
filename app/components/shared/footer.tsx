092ad7004485c985818b255d2196e2db8bdbbc7e
coggiee <zentechie7@gmail.com>
2025-09-15 17:40:43 +0900

    Profile 페이지와 Footer 컴포넌트의 mock 데이터 제거 및 실제 데이터베이스의 데이터로 대체

--- app/components/shared/footer.tsx
+++ app/components/shared/footer.tsx
@@ -2,12 +2,19 @@ import { Link } from "react-router";
 import { appConfig } from "~/config/app-config";
 import HoverSwapLabel from "../animation/hover-swap-label";
 import { cn } from "~/lib/utils";
+import { QueryErrorResetBoundary } from "@tanstack/react-query";
+import { profileQueryOptions } from "~/service/profile";
+import { ErrorBoundary, Suspense } from "@suspensive/react";
+import { SuspenseQuery } from "@suspensive/react-query";
+import { createBrowserClient } from "~/config/supabase-config";
 
 interface FooterProps {
   initBg?: string;
 }
 
 export default function Footer({ initBg }: FooterProps) {
+  const supabase = createBrowserClient();
+
   return (
     <footer
       className={cn(
@@ -15,15 +22,31 @@ export default function Footer({ initBg }: FooterProps) {
         initBg
       )}
     >
-      <ul className="flex justify-start items-center gap-4 md:gap-10">
-        {appConfig.footer.links.map(({ label, href }) => (
-          <li key={label} className="text-sm md:text-base uppercase">
-            <Link to={href}>
-              <HoverSwapLabel text={label} />
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <QueryErrorResetBoundary>
+        {({ reset }) => (
+          <ErrorBoundary onReset={reset} fallback={<div>Error</div>}>
+            <Suspense fallback={<div>Loading</div>}>
+              <SuspenseQuery {...profileQueryOptions.social(supabase)}>
+                {({ data }) => (
+                  <ul className="flex justify-start items-center gap-4 md:gap-10">
+                    {data.map(({ label, href }) => (
+                      <li
+                        key={label}
+                        className="text-sm md:text-base uppercase"
+                      >
+                        <Link to={href}>
+                          <HoverSwapLabel text={label} />
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </SuspenseQuery>
+            </Suspense>
+          </ErrorBoundary>
+        )}
+      </QueryErrorResetBoundary>
+
       <div>
         <p className="text-sm md:text-base">{appConfig.footer.copyright}</p>
       </div>
--- app/routes/_layout.profile.tsx
+++ app/routes/_layout.profile.tsx
@@ -1,5 +1,9 @@
+import { QueryErrorResetBoundary } from "@tanstack/react-query";
 import { Link } from "react-router";
-import { mockConfig } from "~/config/mock-config";
+import { ErrorBoundary, Suspense } from "@suspensive/react";
+import { SuspenseQuery } from "@suspensive/react-query";
+import { profileQueryOptions } from "~/service/profile";
+import { createBrowserClient } from "~/config/supabase-config";
 
 export const handle = {
   navTargetSectionId: "profile-section",
@@ -7,75 +11,92 @@ export const handle = {
 };
 
 export default function ProfileRoute() {
+  const supabase = createBrowserClient();
+
   return (
     <main className="w-full h-fit max-w-[1920px] mx-auto">
       <section
         id={handle.navTargetSectionId}
         className="h-fit flex flex-col justify-center gap-10 px-5 pt-40 pb-10 lg:flex-row lg:gap-0 lg:justify-between w-full lg:h-dvh"
       >
-        <div className="flex flex-col gap-6 basis-2/3">
-          <h1 className="text-[40px] font-medium uppercase tracking-tighter md:text-[56px]">
-            Profile
-          </h1>
+        <QueryErrorResetBoundary>
+          {({ reset }) => (
+            <ErrorBoundary onReset={reset} fallback={<div>Error</div>}>
+              <Suspense fallback={<div>Loading</div>}>
+                <SuspenseQuery {...profileQueryOptions.info(supabase)}>
+                  {({ data }) => (
+                    <>
+                      <div className="flex flex-col gap-6 basis-2/3">
+                        <h1 className="text-[40px] font-medium uppercase tracking-tighter md:text-[56px]">
+                          Profile
+                        </h1>
+                        <div className="flex flex-col gap-6 xl:flex-row xl:gap-16">
+                          {/* Info */}
+                          <div className="flex flex-col gap-2 basis-1/2">
+                            <h2 className="flex gap-2.5">
+                              <span className="font-medium">{data.name}.</span>
+                              <span className="text-[#a6a6a6]">
+                                {data.role}
+                              </span>
+                            </h2>
+                            <p className="font-medium max-w-sm leading-tight">
+                              {data.info}
+                            </p>
+                          </div>
 
-          <div className="flex flex-col gap-6 xl:flex-row xl:gap-16">
-            {/* Info */}
-            <div className="flex flex-col gap-2 basis-1/2">
-              <h2 className="flex gap-2.5">
-                <span className="font-medium">
-                  {mockConfig.profileData.name}.
-                </span>
-                <span className="text-[#a6a6a6]">
-                  {mockConfig.profileData.role}
-                </span>
-              </h2>
-              <p className="font-medium max-w-sm leading-tight">
-                {mockConfig.profileData.info}
-              </p>
-            </div>
+                          {/* Social + Contact */}
+                          <div className="flex flex-col gap-10">
+                            <div>
+                              <h3 className="uppercase text-[#a6a6a6]">
+                                Email & Phone
+                              </h3>
+                              <div className="font-medium">
+                                <Link
+                                  to={`mailto:${data.email}`}
+                                  className="hover-text-muted-foreground"
+                                >
+                                  {data.email}
+                                </Link>
+                                <p className="hover-text-muted-foreground">
+                                  {data.phone}
+                                </p>
+                              </div>
+                            </div>
+                            <div>
+                              <h3 className="uppercase text-[#a6a6a6]">
+                                Socials
+                              </h3>
+                              <div className="flex flex-col font-medium">
+                                {data.social_links.map(({ href, label }) => (
+                                  <Link
+                                    key={label}
+                                    to={href}
+                                    className="hover-text-muted-foreground"
+                                  >
+                                    {label}
+                                  </Link>
+                                ))}
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
 
-            {/* Social + Contact */}
-            <div className="flex flex-col gap-10">
-              <div>
-                <h3 className="uppercase text-[#a6a6a6]">Email & Phone</h3>
-                <div className="font-medium">
-                  <Link
-                    to={`mailto:${mockConfig.profileData.contact.email}`}
-                    className="hover-text-muted-foreground"
-                  >
-                    {mockConfig.profileData.contact.email}
-                  </Link>
-                  <p className="hover-text-muted-foreground">
-                    {mockConfig.profileData.contact.phone}
-                  </p>
-                </div>
-              </div>
-              <div>
-                <h3 className="uppercase text-[#a6a6a6]">Socials</h3>
-                <div className="flex flex-col font-medium">
-                  {mockConfig.profileData.social.map(({ href, label }) => (
-                    <Link
-                      key={label}
-                      to={href}
-                      className="hover-text-muted-foreground"
-                    >
-                      {label}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Profile Image */}
-        <div className="relative lg:w-xs lg:h-[40%] xl:w-1/4 xl:h-[70%]">
-          <img
-            src={mockConfig.profileData.image}
-            alt={mockConfig.profileData.name}
-            className="w-full h-full object-cover"
-          />
-        </div>
+                      {/* Profile Image */}
+                      <div className="relative lg:w-xs lg:h-[40%] xl:w-1/4 xl:h-[70%]">
+                        <img
+                          src={data.image!}
+                          alt={data.name}
+                          className="w-full h-full object-cover"
+                        />
+                      </div>
+                    </>
+                  )}
+                </SuspenseQuery>
+              </Suspense>
+            </ErrorBoundary>
+          )}
+        </QueryErrorResetBoundary>
       </section>
     </main>
   );

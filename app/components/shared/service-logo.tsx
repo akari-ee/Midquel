1e609676fedc9fa52de45a6098c6acb6ad7320a0
coggiee <zentechie7@gmail.com>
2025-09-17 20:46:49 +0900

    서비스 로고 클릭 시 스크롤을 최상단으로 복구하는 로직 추가

--- app/components/shared/service-logo.tsx
+++ app/components/shared/service-logo.tsx
@@ -1,11 +1,22 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import { appConfig } from "~/config/app-config";
 import { routeConfig } from "~/config/route-config";
 
 export default function ServiceLogo() {
+  const location = useLocation();
+
+  const handleClick: React.MouseEventHandler<HTMLAnchorElement> = (e) => {
+    if (location.pathname === routeConfig.HOME.href) {
+      e.preventDefault();
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="font-semibold hover-text-muted-foreground flex-1 md:p-5">
-      <Link to={routeConfig.HOME.href}>{appConfig.serviceDisplayName}</Link>
+      <Link to={routeConfig.HOME.href} onClick={handleClick}>
+        {appConfig.serviceDisplayName}
+      </Link>
     </div>
   );
 }
